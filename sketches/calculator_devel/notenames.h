45f24dd402aac53ccecc0fdabc04adb730a57ffe
Joe Young <jyoung@islandnet.com>
2012-02-23 14:54:22 -0800

    added ben's calc version, silkscreen document

---
+++ sketches/calculator_devel/notenames.h
@@ -0,0 +1,46 @@
+// notenames.h -- musical names for various frequencies.
+// 
+
+#define N_AA  110    
+#define N_A   220    
+#define N_Ash 233
+#define N_B   247
+#define N_C   262
+#define N_Csh 277
+#define N_D   294
+#define N_Dsh 311
+#define N_E   330
+#define N_F   349
+#define N_Fsh 370
+#define N_G   392
+#define N_Gsh 415
+#define N_a   440
+#define N_ash 466
+#define N_b   494
+#define N_c   523
+#define N_csh 554
+#define N_d   587
+#define N_dsh 622
+#define N_e   659
+#define N_f   698
+#define N_fsh 740
+#define N_g   784
+#define N_gsh 831
+#define N_aa  880     
+
+// Flats are like sharps
+#define N_Bb  N_Ash
+#define N_Db  N_Csh
+#define N_Eb  N_Dsh
+#define N_Gb  N_Fsh
+#define N_ab  N_Gsh
+#define N_bb  N_ash
+#define N_db  N_csh
+#define N_eb  N_dsh
+#define N_gb  N_fsh
+#define N_aab N_gsh
+
+
+
+
+
